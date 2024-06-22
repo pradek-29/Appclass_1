48acd282e0e9b341a1ccde5655f69caf88f84169
PRADEK RAJA <pradekraja303@gmail.com>
2024-06-22 15:25:15 +0530

    appclass_2 java to kotlin

--- app/src/main/java/com/example/appclass_1/kotlinegs/myDemo.kt
+++ app/src/main/java/com/example/appclass_1/kotlinegs/myDemo.kt
@@ -2,5 +2,9 @@ package com.example.appclass_1.kotlinegs
 
 fun main(){
     println("hello world")
+    var mystudent:student=student("pradek",12,"chennai")
+    println("name is "+mystudent.name)
+    mystudent.age=20
+    println("age is "+mystudent.age)
 }
 
