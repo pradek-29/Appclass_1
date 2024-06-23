d00e4bc69fc9f9d0bd781ca10bb1ed9adbf18bf7
PRADEK RAJA <pradekraja303@gmail.com>
2024-06-23 12:34:11 +0530

    recyclerview part2-data adapter

---
+++ app/src/main/java/com/example/appclass_1/kotlinegs/DataAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.appclass_1.kotlinegs
+
+import androidx.recyclerview.widget.RecyclerView
+
+class DataAdapter:RecyclerView.Adapter<DataViewHolder>() {
+    class DataViewHolder{
+
+    }
+
+
+}
