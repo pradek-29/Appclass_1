420d885af50caa2a2d23f046e3a6205085d5d97b
PRADEK RAJA <pradekraja303@gmail.com>
2024-06-23 12:04:07 +0530

    spinner

--- app/src/main/java/com/example/appclass_1/HomeActivity.kt
+++ app/src/main/java/com/example/appclass_1/HomeActivity.kt
@@ -2,17 +2,34 @@ package com.example.appclass_1
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    var TAG= HomeActivity::class.java.simpleName
+    lateinit var myspin:Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        myspin=findViewById(R.id.spinner)
+        myspin.onItemSelectedListener=this
 
+    }
+
+    override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var item=adapter?.selectedItem.toString()
+        Log.i(TAG,item)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
